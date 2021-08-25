477b887275837e24f4032a359532da38a1772a54
Prop4et <francostanco97@gmail.com>
2021-08-25 11:19:49 +0200

    started SVMInterpreter

---
+++ SimpLanPlus/src/interpreter/Instruction.java
@@ -0,0 +1,39 @@
+package interpreter;
+
+public class Instruction {
+	private final String instr;
+	private final String arg1;
+    private final int offset;
+    private final String arg2;
+    private final String arg3;
+	
+    public Instruction(String instr, String arg1, int offset, String arg2, String arg3) {
+        this.instr = instr;
+        this.arg1 = arg1;
+        this.offset = offset;
+        this.arg2 = arg2;
+        this.arg3 = arg3;
+    }
+    
+    public String getInstruction() {
+		return instr;
+	}
+
+	public String getArg1() {
+		return arg1;
+	}
+
+	public int getOffset() {
+		return offset;
+	}
+
+	public String getArg2() {
+		return arg2;
+	}
+
+	public String getArg3() {
+		return arg3;
+	}
+    
+    
+}
---
+++ SimpLanPlus/src/interpreter/SVMInterpreter.java
@@ -0,0 +1,51 @@
+package interpreter;
+
+import java.util.List;
+
+public class SVMInterpreter {
+	private final int memSize; // Max size of the memory (heap + stack)
+
+    private final List<Instruction> code;
+    private final int[] memory;
+
+    
+    private int ip = 0;
+    
+    private int sp;
+    private int hp = 0;
+    private int fp;
+    private int ra;
+    private int al;
+    private int a0;
+    private int t1;
+    
+    public SVMInterpreter(int memSize, List<Instruction> code) {
+    	this.memSize = memSize;
+        this.code = code;
+        
+        memory = new int[memSize];
+        
+        sp = memSize;
+        fp = memSize;
+        
+        }
+    
+    public void run() {
+    	while(true) {
+    		if(hp+1>=sp) {
+        		System.out.println("\nError: Out of memory");
+                return;
+        	}else {
+        		Instruction bytecode = code.get(ip); // fetch
+        		ip++;
+        		String arg1 = bytecode.getArg1();
+                String arg2 = bytecode.getArg2();
+                String arg3 = bytecode.getArg3();
+                int offset = bytecode.getOffset();
+                switch(bytecode.getInstruction()) {
+                //missing case, maybe i should wait for the codegen to be completed
+                }
+        	}
+    	}
+    }
+}
